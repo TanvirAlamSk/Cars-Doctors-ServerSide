c8ca288d365bceffca368bacb2022ae4c544d677
Tanvir Alam <ta89725@gmail.com>
2023-08-22 01:17:36 +0600

    only delete remain

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors")
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
@@ -8,11 +8,6 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-
-console.log(process.env.DB_USER)
-
-const products = require("./data/catalog.json")
-const services = require("./data/services.json")
 
 app.get("/", (req, res) => {
     res.send("Server Is Running")
@@ -32,30 +27,80 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-const user = {
-    name: "tanvir",
-    department: "CSE"
-}
+
 
 async function run() {
     try {
-        // const productCollection = client.db("Product").collection("Items")
-        const productCollection = client.db("car-doctor").collection("product")
-        const serviceCollection = client.db("car-doctor").collection("service")
+        const productCollection = client.db("car-doctor").collection("products")
+        const productOrderCollection = client.db("car-doctor").collection("productsOrder")
+        const serviceCollection = client.db("car-doctor").collection("services")
+        const servicePurchaseCollection = client.db("car-doctor").collection("servicesPurchase")
 
+
+
+        // -----------------------service-------------------------------
+        app.get("/services", async (req, res) => {
+            const query = {}
+            const servicefromdb = serviceCollection.find(query)
+            const allservice = await servicefromdb.toArray()
+            res.send(allservice)
+
+        })
+
+        app.get("/service/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const selectedService = await serviceCollection.findOne(query);
+            res.send(selectedService);
+
+        })
+
+        app.post("/service/:id", async (req, res) => {
+            const servicePuschase = req.body
+            const result = await servicePurchaseCollection.insertOne(servicePuschase)
+            res.send(result)
+        })
+
+        app.get("/services/order", async (req, res) => {
+            const query = { email: req.query.email };
+            const servicesOrder = servicePurchaseCollection.find(query);
+            const allOrders = await servicesOrder.toArray(query);
+            res.send(allOrders)
+        })
+
+        // -----------------------product-------------------------------
 
         app.get("/products", async (req, res) => {
             const query = {}
-            const productfromdb = await productCollection.find(query)
+            const productfromdb = productCollection.find(query)
             const allproduct = await productfromdb.toArray()
             res.send(allproduct)
 
         })
 
-        app.post("/products", async (req, res) => {
-            const result = await productCollection.insertOne(user)
-            console.log(result)
+
+        app.get("/product/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const selectedItem = await productCollection.findOne(query)
+            res.send(selectedItem)
+
         })
+        app.post("/product/:id", async (req, res) => {
+            const order = req.body
+            const result = await productOrderCollection.insertOne(order)
+            res.send(result)
+
+        })
+
+        app.get("/products/order", async (req, res) => {
+            const query = { email: req.query.email };
+            const productsOrder = productOrderCollection.find(query);
+            const allOrders = await productsOrder.toArray(query);
+            res.send(allOrders)
+        })
+
+        // -------------------------
 
     } finally {
 
@@ -65,30 +110,6 @@ async function run() {
 }
 run().catch(console.dir);
 //--------------------------data base-------------------------------
-
-
-
-
-
-
-
-app.get("/product", (req, res) => {
-    res.send(products)
-
-})
-
-app.get("/product/:id", (req, res) => {
-    const id = req.params.id
-    const selectedItem = products.find((item) => item._id == id)
-    res.send(selectedItem)
-
-})
-app.get("/service/:id", (req, res) => {
-    const id = req.params.id;
-    const selectedService = services.find((service) => service._id == id);
-    res.send(selectedService);
-
-})
 
 app.listen(port, () => {
     console.log(`Server is runnung on ${port}`)
